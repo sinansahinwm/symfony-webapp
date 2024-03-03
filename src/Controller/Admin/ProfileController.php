b66deeb433bf63489f3e204c2a04fed033cec1a2
meehouweb <sinansahinwm@gmail.com>
2024-03-03 12:42:56 +0300

    profile complete

--- src/Controller/Admin/ProfileController.php
+++ src/Controller/Admin/ProfileController.php
@@ -1,9 +1,14 @@
 <?php namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Form\Auth\Profile\ProfileChangePasswordType;
+use App\Form\Auth\Profile\ProfileEditType;
+use App\Form\Auth\Profile\ProfileKickTeamType;
+use App\Form\Auth\Profile\ProfileMakePassiveType;
 use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -13,7 +18,72 @@ use Twig\Environment;
 #[Route(path: '/admin/profile', name: 'app_admin_profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/switch_to_light_mode', name: 'switch_to_light_mode')]
+
+    #[Route(path: '/', name: 'current')]
+    public function loggedUserProfile(#[CurrentUser] User $loggedUser): Response
+    {
+        return $this->redirectToRoute('app_admin_profile_show', ['theUser' => $loggedUser->getId()]);
+    }
+
+    #[Route(path: '/{theUser}', name: 'show')]
+    public function otherUserProfile(User $theUser): Response
+    {
+        return $this->render('admin/profile/index.html.twig', ["user" => $theUser]);
+    }
+
+    #[Route(path: '/{theUser}/edit', name: 'edit')]
+    public function userEdit(User $theUser, Request $request): Response
+    {
+        $myForm = $this->createForm(ProfileEditType::class, $theUser);
+        $myForm->handleRequest($request);
+
+        if ($myForm->isSubmitted() && $myForm->isValid()) {
+            exit("FORM VALID");
+        }
+
+        return $this->render('admin/profile/edit.html.twig', ["user" => $theUser]);
+    }
+
+    #[Route(path: '/{theUser}/change_password', name: 'change_password')]
+    public function userChangePassword(User $theUser, Request $request): Response
+    {
+        $myForm = $this->createForm(ProfileChangePasswordType::class, $theUser);
+        $myForm->handleRequest($request);
+
+        if ($myForm->isSubmitted() && $myForm->isValid()) {
+            exit("FORM VALID");
+        }
+
+        return $this->render('admin/profile/change_password.html.twig', ["user" => $theUser]);
+    }
+
+    #[Route(path: '/{theUser}/make_passive', name: 'make_passive')]
+    public function userMakePassive(User $theUser, Request $request): Response
+    {
+        $myForm = $this->createForm(ProfileMakePassiveType::class, $theUser);
+        $myForm->handleRequest($request);
+
+        if ($myForm->isSubmitted() && $myForm->isValid()) {
+            exit("FORM VALID");
+        }
+
+        return $this->render('admin/profile/make_passive.html.twig', ["user" => $theUser]);
+    }
+
+    #[Route(path: '/{theUser}/kick_team', name: 'kick_team')]
+    public function userKickTeam(User $theUser, Request $request): Response
+    {
+        $myForm = $this->createForm(ProfileKickTeamType::class, $theUser);
+        $myForm->handleRequest($request);
+
+        if ($myForm->isSubmitted() && $myForm->isValid()) {
+            exit("FORM VALID");
+        }
+
+        return $this->render('admin/profile/kick_team.html.twig', ["user" => $theUser]);
+    }
+
+    #[Route('/current/switch_to_light_mode', name: 'switch_to_light_mode')]
     public function switchToLightMode(#[CurrentUser] User $loggedUser, EntityManagerInterface $entityManager): Response
     {
         $loggedUser->setDarkMode(FALSE);
@@ -22,7 +92,7 @@ class ProfileController extends AbstractController
         return $this->redirectToRoute(LoginFormAuthenticator::SIGNIN_REDIRECT_AFTER_ROUTE);
     }
 
-    #[Route('/switch_to_dark_mode', name: 'switch_to_dark_mode')]
+    #[Route('/current/switch_to_dark_mode', name: 'switch_to_dark_mode')]
     public function switchToDarkMode(#[CurrentUser] User $loggedUser, EntityManagerInterface $entityManager): Response
     {
         $loggedUser->setDarkMode(TRUE);
@@ -31,7 +101,7 @@ class ProfileController extends AbstractController
         return $this->redirectToRoute(LoginFormAuthenticator::SIGNIN_REDIRECT_AFTER_ROUTE);
     }
 
-    #[Route('/switch_locale_to/{fallback}', name: 'switch_locale_to')]
+    #[Route('/current/switch_locale_to/{fallback}', name: 'switch_locale_to')]
     public function switchLocaleTo(#[CurrentUser] User $loggedUser, ?string $fallback, EntityManagerInterface $entityManager, Environment $twig): Response
     {
         $validFallbacks = $twig->getGlobals()["layout"]["locales"];
---
+++ src/Form/Auth/Profile/ProfileChangePasswordType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form\Auth\Profile;
+
+use App\Entity\Team;
+use App\Entity\User;
+use App\Form\AbstractFormType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileChangePasswordType extends AbstractFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('display_name')
+            ->add('phone')
+            ->add('created_at')
+            ->add('isVerified')
+            ->add('dark_mode')
+            ->add('locale')
+            ->add('team', EntityType::class, [
+                'class' => Team::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
