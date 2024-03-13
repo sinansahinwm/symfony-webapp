9985bf4c7cb9c76516094b92cea9fac47afecca9
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-13 17:54:55 +0300

    app spec uploaded

--- assets/cloud_functions/index.js
+++ assets/cloud_functions/index.js
@@ -1,8 +1,16 @@
+// Mandatory Imports
 import functions from '@google-cloud/functions-framework';
 
-// Puppeteer Replayer
-functions.http('puppeteerReplayer', _puppeteerReplayerHandler);
+// Library Imports
+import puppeteer from "puppeteer";
+import {v4 as uuidv4} from 'uuid';
+import axios from 'axios';
+import {PuppeteerRunnerExtension} from "@puppeteer/replay";
 
-function _puppeteerReplayerHandler(request, response) {
+// Define Functions
+functions.http('puppeteerReplayer', _puppeteerReplayer);
+
+// Define Function Handlers
+function _puppeteerReplayer(request, response) {
     response.send("Hello from pup");
 }
