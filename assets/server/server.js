31ecd58a6358f413b715953731deb636af75b214
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-13 11:20:46 +0300

    server.js uploaded

--- assets/server/server.js
+++ assets/server/server.js
@@ -4,9 +4,10 @@ import 'dotenv/config'
 import {configDotenv} from "dotenv";
 
 // Define App
-const dotEnv = configDotenv({path: "../../.env"}).parsed;
+const dotEnv = configDotenv({ path: "../../.env" }).parsed;
 const myServer = express();
-const myServerPort = dotEnv.NODE_SERVER_PORT;
+const myServerPort = 3030;
+console.log(dotEnv);
 
 myServer.listen(myServerPort, () =>
     console.log('Node Server Started On Port: ' + myServerPort),
