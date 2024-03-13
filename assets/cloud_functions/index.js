f76540de5887d154fd96950a9484ce5c02b93ce8
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-13 18:39:06 +0300

    puppeteer replayer node function added

--- assets/cloud_functions/index.js
+++ assets/cloud_functions/index.js
@@ -2,16 +2,52 @@
 import functions from '@google-cloud/functions-framework';
 
 // Library Imports
+import {createRunner} from '@puppeteer/replay';
 import puppeteer from "puppeteer";
-import {v4 as uuidv4} from 'uuid';
-import axios from 'axios';
-import {PuppeteerRunnerExtension} from "@puppeteer/replay";
+
+// Vendor Imports
+import PuppeteerBridgeExtension from './vendor/extension.js';
 
 // Define Functions
 functions.http('puppeteerReplayer', _puppeteerReplayer);
 
 // Define Function Handlers
-function _puppeteerReplayer(request, response) {
-    response.send("Hello from pup");
-    console.log(request);
+async function _puppeteerReplayer(request, response) {
+
+    const requestAppSecret = request.headers["X-Authorization-AppSecret"];
+    const validAppSecret = "8c9db0e6d88f9190ac9a001fadaf1e8d";
+    const requestContentType = request.get('Content-Type').toLowerCase();
+
+    // Check Content Type & Authorization
+    if ((requestContentType === "application/json") && (requestAppSecret === validAppSecret)) {
+
+        // Get Data
+        const requestBody = request.body;
+        const instanceID = requestBody.instanceID;
+        const webhookURL = requestBody.webhookURL;
+        const timeOut = requestBody.timeOut;
+        const puppeteerLaunchOptions = requestBody.puppeteerLaunchOptions;
+        const puppeteerSteps = requestBody.steps;
+
+        // Create Puppeteer Instance
+        const myBrowser = await puppeteer.launch(puppeteerLaunchOptions);
+        const myPage = await myBrowser.newPage();
+
+        const myFlow = {
+            title: instanceID,
+            steps: puppeteerSteps,
+        };
+        const myRunner = await createRunner(
+            myFlow,
+            new PuppeteerBridgeExtension(myBrowser, myPage, timeOut, webhookURL, instanceID)
+        );
+
+        // Run & Dispose
+        await myRunner.run();
+        await myBrowser.close();
+
+        response.send("OK");
+    }
+
+    response.next();
 }
