18c9721bcace64d29af362775853d001d95b30f2
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-13 10:05:17 +0300

    server node system changed

--- assets/server/server.js
+++ assets/server/server.js
@@ -1,12 +1,17 @@
+// Add Imports
 import express from 'express';
+import 'dotenv/config'
+import {configDotenv} from "dotenv";
 
+// Define App
+const dotEnv = configDotenv({path: "../../.env"}).parsed;
 const myServer = express();
-const myServerPort = 3030;
+const myServerPort = dotEnv.NODE_SERVER_PORT;
 
 myServer.listen(myServerPort, () =>
-    console.log('Puppeteer Replayer Server Started On Port: ' + myServerPort),
+    console.log('Node Server Started On Port: ' + myServerPort),
 );
 
 myServer.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
--- src/Service/PuppeteerReplay/PuppeteerReplayPackager.php
+++ src/Service/PuppeteerReplay/PuppeteerReplayPackager.php
@@ -7,6 +7,9 @@ use ZipArchive;
 class PuppeteerReplayPackager
 {
 
+    const NODE_APP_NAME = 'puppeteer_replayer';
+    const NODE_PACKAGE_START_ENTRYPOINT = 'index.js';
+    const NODE_PACKAGE_GLOB = "/*.*";
     private PuppeteerReplayCombinator $replayCombinator;
 
     public function __construct(PuppeteerReplayCombinator $replayCombinator, private PuppeteerReplayLoader $replayLoader, private ContainerBagInterface $containerBag)
@@ -35,15 +38,15 @@ class PuppeteerReplayPackager
     private function preparePackage(string $combinedJS)
     {
         $myFS = new Filesystem();
-        $zipFilename = $myFS->tempnam('/tmp', 'puppeteer_replayer_package_', '.zip');
-        $replayerPackageBaseDir = $this->containerBag->get("app.projectDir") . "/assets/puppeteer_replayer";
+        $zipFilename = $myFS->tempnam('/tmp', self::NODE_APP_NAME . "_", '.zip');
+        $replayerPackageBaseDir = $this->containerBag->get("app.projectDir") . "/assets/server/apps/" . self::NODE_APP_NAME;
         $packageZip = new ZipArchive();
         if ($packageZip->open($zipFilename, ZipArchive::CREATE) === TRUE) {
-            foreach (glob($replayerPackageBaseDir . "/*.*") as $packageFile) {
+            foreach (glob($replayerPackageBaseDir . self::NODE_PACKAGE_GLOB) as $packageFile) {
                 $basename = basename($packageFile);
                 $packageZip->addFromString($basename, file_get_contents($packageFile));
             }
-            $packageZip->addFromString('index.js', $combinedJS);
+            $packageZip->addFromString(self::NODE_PACKAGE_START_ENTRYPOINT, $combinedJS);
             $packageZip->close();
         }
         return $zipFilename;
