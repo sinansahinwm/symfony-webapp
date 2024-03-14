88711d4c50c52a38a6f6b27f4000ca27fb165a77
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-14 14:13:49 +0300

    headless true changed

--- src/Controller/Webhook/PuppeteerReplayerWebhook.php
+++ src/Controller/Webhook/PuppeteerReplayerWebhook.php
@@ -1,6 +1,5 @@
 <?php namespace App\Controller\Webhook;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
