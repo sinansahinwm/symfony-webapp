254982f861a782a56f4936c242a6a4f0553df477
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-14 16:36:41 +0300

    puppeteer replay entity added

--- src/EventListener/PuppeteerReplayListener.php
+++ src/EventListener/PuppeteerReplayListener.php
@@ -1,25 +1,36 @@
 <?php namespace App\EventListener;
 
+use App\Controller\Webhook\PuppeteerReplayerWebhook;
 use App\Entity\PuppeteerReplay;
+use App\Service\PuppeteerReplayService;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Events;
+use Exception;
+use Psr\Log\LoggerInterface;
 use Vich\UploaderBundle\Storage\StorageInterface;
 
 #[AsEntityListener(event: Events::postPersist, method: "postPersist", entity: PuppeteerReplay::class)]
 class PuppeteerReplayListener
 {
-    public function __construct(private StorageInterface $storage)
+    public function __construct(private StorageInterface $storage, private LoggerInterface $logger, private PuppeteerReplayService $puppeteerReplayService, private PuppeteerReplayerWebhook $puppeteerReplayerWebhook)
     {
     }
 
     public function postPersist(PuppeteerReplay $puppeteerReplay): void
     {
+        try {
+            $resolvedFilePath = $this->storage->resolvePath($puppeteerReplay);
 
-        $resolvedFilePath = $this->storage->resolvePath($puppeteerReplay);
+            $this->puppeteerReplayService
+                ->setRecordPath($resolvedFilePath)
+                ->setWebhook($this->puppeteerReplayerWebhook)
+                ->setInstanceID($puppeteerReplay->getId())
+                ->play();
 
-
-        exit($resolvedFilePath);
+        } catch (Exception $exception) {
+            $this->logger->error($exception);
+        }
     }
 
 }
