b273070b4c128b05f6407bff38d408f4a4b50e8a
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-15 13:36:08 +0300

    show more text column added

--- src/Service/CrudTable/ShowMoreTextColumn.php
+++ src/Service/CrudTable/ShowMoreTextColumn.php
@@ -5,7 +5,7 @@ use Omines\DataTablesBundle\Column\TextColumn;
 class ShowMoreTextColumn extends TextColumn
 {
     const SLICE_THRESHOLD = 10;
-    const SLICE_SHOW_MORE_DELIMETER = '';
+    const SLICE_SHOW_MORE_DELIMETER = '...';
     const SLICE_SHOW_MORE_SPAN_CLASS = ['text-primary', 'ms-2'];
 
     public function normalize(mixed $value): string
