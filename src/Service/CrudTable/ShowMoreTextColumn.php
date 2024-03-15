13e8dea9def4391415337cf5aaa38fe9ed162ba4
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-03-15 13:31:57 +0300

    show more text column added

--- src/Service/CrudTable/ShowMoreTextColumn.php
+++ src/Service/CrudTable/ShowMoreTextColumn.php
@@ -5,14 +5,22 @@ use Omines\DataTablesBundle\Column\TextColumn;
 class ShowMoreTextColumn extends TextColumn
 {
     const SLICE_THRESHOLD = 10;
+    const SLICE_SHOW_MORE_DELIMETER = '';
+    const SLICE_SHOW_MORE_SPAN_CLASS = ['text-primary', 'ms-2'];
 
     public function normalize(mixed $value): string
     {
         $normalizedParent = parent::normalize($value);
-        if (strlen($normalizedParent) > self::SLICE_THRESHOLD) {
-            return  "slicing" . $value;
+        if (strlen($normalizedParent) > self::SLICE_THRESHOLD && is_string($value)) {
+            $slicedText = mb_substr($value, 0, self::SLICE_THRESHOLD);
+            return $slicedText . $this->getSlicedPostFix($value);
         }
         return $value;
+    }
+
+    private function getSlicedPostFix(string $value): string
+    {
+        return '<span class="showMoreSpan ' . implode(' ', self::SLICE_SHOW_MORE_SPAN_CLASS) . '" data-content="' . $value . '">' . self::SLICE_SHOW_MORE_DELIMETER . '</span>';
     }
 
 }
