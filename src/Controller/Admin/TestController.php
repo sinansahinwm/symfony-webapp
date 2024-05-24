5d8a63e6855a04523920d9203bcae4a07a92d927
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-05-24 09:25:14 +0300

    clearfix

--- src/Controller/Admin/TestController.php
+++ src/Controller/Admin/TestController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controller\Admin;
 
+use App\Service\WebScrapingRequestService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -8,9 +9,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class TestController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(WebScrapingRequestService $webScrapingRequestService): Response
     {
-        exit("TEST");
+        foreach (range(100, 1000, 100) as $index => $item) {
+            $webScrapingRequestService->createRequest('https://placehold.co/100x' . $index . '/png');
+        }
+
+        exit("s");
     }
 
 }
