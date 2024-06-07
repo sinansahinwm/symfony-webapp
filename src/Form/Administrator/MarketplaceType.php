f48b7e79531ac6071b3ecc3e6b66e6444ace7248
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-06-07 15:28:22 +0300

    marketplace new fields added

--- src/Form/Administrator/MarketplaceType.php
+++ src/Form/Administrator/MarketplaceType.php
@@ -1,7 +1,10 @@
 <?php namespace App\Form\Administrator;
 
+use App\Config\MarketplaceSearchHandlerType;
 use App\Entity\Marketplace;
 use App\Form\AbstractFormType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,6 +24,18 @@ class MarketplaceType extends AbstractFormType
             ])
             ->add('logo', UrlType::class, [
                 'label' => t("Logo")
+            ])
+            ->add('search_url', UrlType::class, [
+                'label' => t("Arama URL")
+            ])
+            ->add('search_selectors', TextareaType::class, [
+                'label' => t("Arama Seçicileri"),
+                'help' => t("Ve Bağlacı İçin Şunu Kullanın: ") . Marketplace::SEARCH_SELECTORS_SPLITTER,
+                'required' => FALSE
+            ])
+            ->add('search_handler_type', ChoiceType::class, [
+                'label' => t("Arama Yöntemi"),
+                'choices' => MarketplaceSearchHandlerType::getValuesArray()
             ]);
     }
 
