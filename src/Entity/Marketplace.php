b61fadcdd83c03a0a99070af4d0cf2c055ea8340
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-06-07 15:29:27 +0300

    marketplace new fields and delimeters added

--- src/Entity/Marketplace.php
+++ src/Entity/Marketplace.php
@@ -3,11 +3,17 @@
 namespace App\Entity;
 
 use App\Repository\MarketplaceRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MarketplaceRepository::class)]
 class Marketplace
 {
+
+    const SEARCH_SELECTORS_SPLITTER = '<!-- AND -->';
+    const SEARCH_URL_PLACEHOLDER_START = '{{';
+    const SEARCH_URL_PLACEHOLDER_END = '}}';
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -21,6 +27,15 @@ class Marketplace
 
     #[ORM\Column(length: 255)]
     private ?string $logo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $search_url = null;
+
+    #[ORM\Column(type: 'marketplace_search_handler')]
+    private $search_handler_type = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $search_selectors = null;
 
     public function getId(): ?int
     {
@@ -59,6 +74,57 @@ class Marketplace
     public function setLogo(string $logo): static
     {
         $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
+    public function getSearchUrl(): ?string
+    {
+        return $this->search_url;
+    }
+
+    public function getRealSearchUrl(array $dataContext = []): ?string
+    {
+        $myRawSearchURL = $this->getSearchUrl();
+        foreach ($dataContext as $dataKey => $dataValue) {
+            $theSelectorPlaceholder = self::SEARCH_URL_PLACEHOLDER_START . $dataKey . self::SEARCH_URL_PLACEHOLDER_END;
+            $myRawSearchURL = str_replace($theSelectorPlaceholder, $dataValue, $myRawSearchURL);
+        }
+        return $myRawSearchURL;
+    }
+
+    public function setSearchUrl(string $search_url): static
+    {
+        $this->search_url = $search_url;
+
+        return $this;
+    }
+
+    public function getSearchHandlerType()
+    {
+        return $this->search_handler_type;
+    }
+
+    public function setSearchHandlerType($search_handler_type): static
+    {
+        $this->search_handler_type = $search_handler_type;
+
+        return $this;
+    }
+
+    public function getSearchSelectors(): ?string
+    {
+        return $this->search_selectors;
+    }
+
+    public function setSearchSelectors(?string $search_selectors): static
+    {
+        $this->search_selectors = $search_selectors;
 
         return $this;
     }
