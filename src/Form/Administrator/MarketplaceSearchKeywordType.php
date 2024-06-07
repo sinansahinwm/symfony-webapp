1887b6425651d5a80ada96de832f3c4310c74b93
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-06-07 15:28:46 +0300

    marketplace search keyword type added

---
+++ src/Form/Administrator/MarketplaceSearchKeywordType.php
@@ -0,0 +1,37 @@
+<?php namespace App\Form\Administrator;
+
+use App\Entity\Marketplace;
+use App\Form\AbstractFormType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Symfony\Component\Translation\t;
+
+class MarketplaceSearchKeywordType extends AbstractFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('keyword', TextType::class, [
+                'label' => t("Arama Kelimesi"),
+                'mapped' => FALSE
+            ])
+            ->add('marketplaces', EntityType::class, [
+                'label' => t("Pazaryerleri"),
+                'class' => Marketplace::class,
+                'multiple' => TRUE,
+                'mapped' => FALSE,
+                'attr' => [
+                    'class' => "select2",
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // 'data_class' => Marketplace::class,
+        ]);
+    }
+}
