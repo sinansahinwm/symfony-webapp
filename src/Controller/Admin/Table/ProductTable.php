48a451430bd9979a07222d7dce5b62922fd6831d
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-06-12 17:19:03 +0300

    actions added

--- src/Controller/Admin/Table/ProductTable.php
+++ src/Controller/Admin/Table/ProductTable.php
@@ -1,15 +1,17 @@
 <?php namespace App\Controller\Admin\Table;
 
 use App\Entity\Product;
+use App\Service\CrudTable\ActionsColumn;
+use App\Service\CrudTable\CrudTableAction;
 use App\Service\CrudTable\DisableCachingCriteriaProvider;
 use App\Service\CrudTable\ImageColumn;
-use App\Service\CrudTable\ShowMoreTextColumn;
 use Doctrine\ORM\QueryBuilder;
 use Omines\DataTablesBundle\Adapter\Doctrine\ORM\SearchCriteriaProvider;
 use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
 use Omines\DataTablesBundle\Column\TextColumn;
 use Omines\DataTablesBundle\DataTable;
 use Omines\DataTablesBundle\DataTableTypeInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductTable extends TableAbstractController implements DataTableTypeInterface
 {
@@ -31,10 +33,23 @@ class ProductTable extends TableAbstractController implements DataTableTypeInter
             'label' => $this->t("Kimlik"),
             'orderable' => FALSE
         ]);
-        $dataTable->add('name', ShowMoreTextColumn::class, [
+        $dataTable->add('name', TextColumn::class, [
             'label' => $this->t("Ad"),
             'orderable' => FALSE,
-            'slice' => 50
+        ]);
+
+        $dataTable->add('id', ActionsColumn::class, [
+            'target_blank' => TRUE,
+            'actions' => [
+                function ($value, UrlGeneratorInterface $urlGenerator) {
+                    $theURL = $urlGenerator->generate('app_administrator_product_go_to_product', ['theProduct' => $value]);
+                    return new CrudTableAction($this->t("Ürüne Git"), $theURL, 'bx bx-link');
+                },
+                function ($value, UrlGeneratorInterface $urlGenerator) {
+                    $theURL = $urlGenerator->generate('app_administrator_product_go_to_product_marketplace', ['theProduct' => $value]);
+                    return new CrudTableAction($this->t("Pazaryerine Git"), $theURL, 'bx bx-store-alt');
+                },
+            ]
         ]);
 
         $dataTable->createAdapter(ORMAdapter::class, [
--- src/Controller/Administrator/ProductController.php
+++ src/Controller/Administrator/ProductController.php
@@ -1,8 +1,10 @@
 <?php namespace App\Controller\Administrator;
 
 use App\Controller\Admin\Table\ProductTable;
+use App\Entity\Product;
 use App\Service\CrudTable\CrudTableService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,6 +21,18 @@ class ProductController extends AbstractController
         return $this->render('administrator/product/index.html.twig', [
             'productTable' => $webScrapingRequestTable,
         ]);
+    }
+
+    #[Route('/go_to_product/{theProduct}', name: 'go_to_product')]
+    public function goToProduct(Product $theProduct): RedirectResponse
+    {
+        return $this->redirect($theProduct->getUrl());
+    }
+
+    #[Route('/go_to_product_marketplace/{theProduct}', name: 'go_to_product_marketplace')]
+    public function goToProductMarketplace(Product $theProduct): RedirectResponse
+    {
+        return $this->redirect($theProduct->getMarketplace()->getUrl());
     }
 
 }
