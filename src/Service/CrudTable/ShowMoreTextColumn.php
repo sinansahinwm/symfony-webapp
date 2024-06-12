2522139d2e1ba61b1fa3f668b014b07811b9655e
Sinan ŞAHİN <sinansahinwm@gmail.com>
2024-06-12 16:46:33 +0300

    slice size added

--- src/Service/CrudTable/ShowMoreTextColumn.php
+++ src/Service/CrudTable/ShowMoreTextColumn.php
@@ -1,6 +1,7 @@
 <?php namespace App\Service\CrudTable;
 
 use Omines\DataTablesBundle\Column\TextColumn;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ShowMoreTextColumn extends TextColumn
 {
@@ -10,9 +11,10 @@ class ShowMoreTextColumn extends TextColumn
 
     public function normalize(mixed $value): string
     {
+        $sliceSizeThreshold = $this->options["slice"] ?? self::SLICE_THRESHOLD;
         $normalizedParent = parent::normalize($value);
-        if (strlen($normalizedParent) > self::SLICE_THRESHOLD && is_string($value)) {
-            $slicedText = mb_substr($value, 0, self::SLICE_THRESHOLD);
+        if (strlen($normalizedParent) > $sliceSizeThreshold && is_string($value)) {
+            $slicedText = mb_substr($value, 0, $sliceSizeThreshold);
             return $slicedText . $this->getSlicedPostFix($value);
         }
         return $value;
@@ -21,6 +23,16 @@ class ShowMoreTextColumn extends TextColumn
     private function getSlicedPostFix(string $value): string
     {
         return '<span class="showMoreSpan ' . implode(' ', self::SLICE_SHOW_MORE_SPAN_CLASS) . '" data-content="' . $value . '">' . self::SLICE_SHOW_MORE_DELIMETER . '</span>';
+    }
+
+    protected function configureOptions(OptionsResolver $resolver): static
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefault('slice', null);
+        $resolver->setAllowedTypes('slice', ['null', 'int']);
+
+        return $this;
     }
 
 }
